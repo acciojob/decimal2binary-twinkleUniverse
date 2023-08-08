edac9687ffb3120d2fe749b6b812263d0184fa14
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-08-08 16:53:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function decimalToBinary(num) {
   //Write you code here
   let text=num.toString(2);
-	console.log(text);
+	console.log(text)
 }
 
-window.decimalToBinary = decimalToBinary;
+window.decimalToBinary = decimalToBinary(10);
